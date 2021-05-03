e997961f95bfcb45a633f08ebf979263d2b1ba4d
sjh001111 <sjh001111@gmail.com>
2021-05-03 20:46:05 +1000

    기본 틀

---
+++ function_prototypes.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Function prototypes
+*******************************************************************************/
+
+void print_menu();
+void add_user();
+void display_users();
+void save_users();
+void read_users();
+void search_users();
+void sort_users();
+char* xor_encryption(char* data);
+char* xor_decryption(char* data);
+char* run_length_compression(char* data);
+char* run_length_decompression(char* data);
