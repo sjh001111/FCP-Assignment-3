893bf96be0135affd0c1efb6e623a08ffbb43832
hduk2038 <49715836+hduk2038@users.noreply.github.com>
2021-05-21 13:15:46 +0900

    Update rle.c

--- libs/rle.c
+++ libs/rle.c
@@ -59,38 +59,3 @@ char *run_length_decompression(char *input)
     output[j] = '\0';
     return output;
 }
-
-/* 에러남
-char *run_length_decompression(char *input)
-{
-    char* decompression;
-    int decompression_length = run_length_compression(input);
-    decompression = (char*)malloc(decompression_length * sizeof(char*) + 1);
-
-    char point;
-    int count;
-    int i;
-    int j;
-    int current_Length_Index = 0;
-
-    for (i = 0; i < strlen(input) - 1; i++) {
-
-        if ((input + i) != '\0') {
-
-            sscanf(input + i, "%c%u", &point, &count);
-
-            for (j = 0; j < count; j++) {
-                *(decompression + current_Length_Index + j) = point;
-            }
-        }
-
-        current_Length_Index += j;
-        i += floor(log10(abs(count))) + 1;
-    }
-
-    *(decompression + decompression_length + 1) = '\0';
-
-    return decompression;
-}
-}
-*/
