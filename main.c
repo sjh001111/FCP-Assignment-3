6bffe84558c5de36e9e5bf94c051316ec63300a9
sjh001111 <sjh001111@gmail.com>
2021-05-25 18:01:10 +1000

    2021.05.25.03

--- main.c
+++ main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 #include "user.h"
 
-void print_menu();
-int menu(User users[], int *count, int i);
+void print_menu(int debug);
+int menu(User users[], int *count, int i, int debug);
 
-int main()
+int main(int argc, char *argv[])
 {
     User users[MAX_USERS_SIZE];
-    int i, count = 0;
+    int i, count = 0, debug = 0;
+
+    if (argc > 1 && !strcmp(argv[1], "debug"))
+        debug = 1;
 
     do
     {
-        print_menu();
+        print_menu(debug);
         scanf("%d", &i);
         getchar();
         printf("\n");
-    } while (menu(users, &count, i));
+    } while (menu(users, &count, i, debug));
 
     return 0;
 }
 
-void print_menu()
+void print_menu(int debug)
 {
     printf(
         "旨收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收收旬\n"
@@ -30,14 +34,17 @@ void print_menu()
         "弛 2. Display users (XOR decryption, caesar decryption, Sorting, Searching) 弛\n"
         "弛 3. Save users (Run-length compression)                                   弛\n"
         "弛 4. Read users (Run-length decompression)                                 弛\n"
-        "弛 5. Delete user                                                           弛\n"
-        "弛 6. Debug                                                                 弛\n"
+        "弛 5. Delete user                                                           弛\n");
+    if (debug)
+        printf(
+            "弛 6. Debug                                                                 弛\n");
+    printf(
         "弛 7. Exit app                                                              弛\n"
         "戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n"
         "  Enter the number: ");
 }
 
-int menu(User users[], int *count, int i)
+int menu(User users[], int *count, int i, int debug)
 {
     switch (i)
     {
@@ -77,11 +84,14 @@ int menu(User users[], int *count, int i)
         delete_user(users, count);
         break;
     case 6:
-        printf(
-            "忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n"
-            "弛 6. Debug                                                                 弛\n"
-            "戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n");
-        debug(users, count);
+        if (debug)
+        {
+            printf(
+                "忙式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式忖\n"
+                "弛 6. Debug                                                                 弛\n"
+                "戌式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式戎\n");
+            debug(users, count);
+        }
         break;
     case 7:
         return 0;
