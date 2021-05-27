a50a2752269753bf831a7696e4bdebc6b9654543
joh887 <77087931+joh887@users.noreply.github.com>
2021-05-27 23:47:37 +0900

    Update radix_sort.c

--- libs/radix_sort.c
+++ libs/radix_sort.c
@@ -2,6 +2,7 @@
 #include "../user.h"
 #include "radix_sort.h"
 
+//compares and return max
 int get_max(User users[], int count)
 {
     int i, max = users[0].ID;
@@ -12,24 +13,25 @@ int get_max(User users[], int count)
     return max;
 }
 
+//sorts the elements inside
 void count_sort(User users[], int count, int value)
 {
     int i, cnt[10] = {0};
     User sorted_users[count];
-
+//count the number of elements
     for (i = 0; i < count; i++)
         cnt[(users[i].ID / value) % 10]++;
-
+//place the cumulative sum inside count
     for (i = 1; i < 10; i++)
         cnt[i] += cnt[i - 1];
 
-
+//sort by correct order
     for (i = count - 1; i >= 0; i--)
     {
         sorted_users[cnt[(users[i].ID / value) % 10] - 1] = users[i];
         cnt[(users[i].ID / value) % 10]--;
     }
-
+//replace the users[] by the new sorted one
     for (i = 0; i < count; i++)
         users[i] = sorted_users[i];
 }
