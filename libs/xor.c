8a38573840444f4f15caa56484a888d5c21899b2
joh887 <77087931+joh887@users.noreply.github.com>
2021-05-28 01:17:48 +0900

    Update xor.c

--- libs/xor.c
+++ libs/xor.c
@@ -3,23 +3,24 @@
 #include "../user.h"
 #include "xor.h"
 
-// xor 암호화 & 복호화
+// XOR encryption & decryption
 char *XOR_cipher(char *input)
 {
-    // 입력받은 문자열의 길이보다 1만큼 큰 문자열을 생성 (임의로 생성하는 문자열은 마지막에 '\0'를 넣어줘야해서)
+    /* Generate a char[] that is 1 greater than the length 
+    of the input char[] (because temporary char[] needs'\0' at the end)*/
     int i, j = 0, length = strlen(input);
     char *output = (char *)malloc(sizeof(char) * (length + 1));
-    // S, J, H라는 키 생성
+    // Make a key called S, J, H
     char key[] = {'S', 'J', 'H'};
 
     for (i = 0; i < strlen(input); i++)
     {
-        // xor 연산
+        // XOR calculation
         output[i] = input[i] ^ key[i % (sizeof(key) / sizeof(char))];
         j++;
     }
 
-    // 아웃풋 마지막에 '\0' 넣어서 문자열 완성~
+    // Put '\0' at the end of the output to finish
     output[j] = '\0';
     return output;
 }
