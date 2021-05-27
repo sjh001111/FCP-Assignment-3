4c5bf727b13485962962d238bf97f25b53e5e087
joh887 <77087931+joh887@users.noreply.github.com>
2021-05-27 23:37:35 +0900

    Update binary_search.c

--- libs/binary_search.c
+++ libs/binary_search.c
@@ -2,13 +2,15 @@
 #include "../user.h"
 #include "binary_search.h"
 
+//search by binary search algorithm
 int binary_search(User users[], int count, int search)
 {
    int start, end, center;
    start = 0;
    end = count - 1;
    center = (start + end) / 2;
-
+/* Divides in half and compares until 
+the center matches searching value*/
    while (start <= end)
    {
       if (users[center].ID < search)
